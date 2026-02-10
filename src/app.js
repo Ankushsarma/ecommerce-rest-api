68f68af484a30a48e1c7db3bee44dbdf138f6b1c
Ankushsarma <ankushsharma5524@gmail.com>
2026-02-10 22:39:40 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app =express();
+app.use(express.json());
+
+
+app.use('/auth',require("./routes/authRoutes"));
+app.use("/users" ,require("./routes/userRoutes"));
+app.use("/products",require("./routes/productRoutes"));
+app.use("/cart",require("./routes/cartRoutes"));
+app.use("/orders",require("./routes/orderRoutes"));
+app.use("/reviews",require("./routes/reviewRoutes"));
+app.use("/wishlist",require("./routes/wishlistRoutes"));
+app.use("/payment",require("./routes/paymentRoutes"));
+
+
+
+
+module.exports=app;

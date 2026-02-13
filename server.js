081efd2bfd2b42ea34473927d3d1fdcee89b34c4
Ankushsarma <ankushsharma5524@gmail.com>
2026-02-14 01:05:44 +0530

    mongodb and jwt setup and user model created

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
+
 const app=require('./src/app')
+const connectDB = require("./src/config//db")
 
+const port = process.env.Port || 5000;
 
-app.listen(3000,()=>{
+connectDB();
+
+app.listen(port,()=>{
     console.log("server is running");
 })
---
+++ src/models/userModel.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const bycrypt = require("bcryptjs");
+const { resetPassword } = require("../contoller/authController");
+
+const userSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            require:true
+        },
+        email:{
+            type:String,
+            require:true,
+            unique:true
+        },
+        password:{
+            type:String,
+            require:true,
+            minlength:6,
+            select:fales  // new thing
+        },
+        role:{
+            type:String,
+            enum:["user","admin","seller"],
+            default:"user"
+        },
+        resetpasswordToken:String,
+        resetPasswordExpire:Date,
+    },
+    { timestamps:true} // new thing
+)
+
+// password hashing
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+    this.password=await bycrypt.hash(this.password,10);
+    next()
+})
+
+// compare password
+userSchema.method.comparePassword=async function(password){
+    return bycrypt.compare(password,this.password);
+}
+
+module.exports =mongoose.model("user",userSchema);

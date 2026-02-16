5c2c2b6f22b208fc24c009b051a7a438f65d968f
Ankushsarma <ankushsharma5524@gmail.com>
2026-02-16 13:33:21 +0530

    product model updated

--- src/models/productModule.js
+++ src/models/productModule.js
@@ -7,7 +7,7 @@ const productSchema = new mongoose.Schema(
     price: { type: Number, required: true },
     stock: { type: Number, default: 0 },
     category: String,
-    images: [String],
+    images: String,
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
